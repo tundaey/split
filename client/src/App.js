66573d0c9c8c35dd08e37847662ae860a4eda3fb
tundaey <tundaey@gmail.com>
2019-09-24 14:04:58 +0100

    intial styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import './app.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <h2 data-testid="my-container">Hello Tunde I changed something on the feature branch!</h2>
-      </React.Fragment>
+      <div>
+        <nav className="Nav">
+          <div className="Nav__logo">Logo</div>
+          <ul className="Nav__center">
+            <li>Dashboard</li>
+            <li>Providers</li>
+            <li>Users</li>
+            <li>Settings</li>
+          </ul>
+          <a href="#">New Provider</a>
+          <div className="Nav__user">
+            <img className="Nav__user-photo" src="#"/>
+            <span className="Nav__user-name">Tunde</span>
+          </div>
+        </nav>
+        <div>Summary</div>
+        <div>
+          <div>Recent Transactions</div>
+          <div>All Transactions</div>
+        </div>
+      </div>
     )
   }
 }
