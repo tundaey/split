a2f9c453d240c27e694b6cb8c8828714a7027c07
tundaey <tundaey@gmail.com>
2019-09-25 07:35:10 +0100

    intial design

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './app.scss';
+import userImg from './tee.jpeg'
 
 class App extends React.Component {
   render() {
@@ -13,13 +14,32 @@ class App extends React.Component {
             <li>Users</li>
             <li>Settings</li>
           </ul>
-          <a href="#">New Provider</a>
+          <div className="Nav__action">
+            <a className="Button" href="#">New Provider</a>
+          </div>
           <div className="Nav__user">
-            <img className="Nav__user-photo" src="#"/>
-            <span className="Nav__user-name">Tunde</span>
+            <img className="Nav__user-photo" src={userImg}/>
           </div>
         </nav>
-        <div>Summary</div>
+        <div className="Summary">
+          <div className="Summary__item">
+            <p className="Summary__item--label">overdue</p>
+            <p className="Summary__item--value">£500</p>
+            <a className="Button Button__small" href="#">View all</a>
+          </div>
+
+          <div className="Summary__item">
+            <p className="Summary__item--label">in draft</p>
+            <p className="Summary__item--value">£3400</p>
+            <a className="Button Button__small" href="#">View all</a>
+          </div>
+
+          <div className="Summary__item">
+            <p className="Summary__item--label">total outstanding</p>
+            <p className="Summary__item--value">£3900</p>
+            <a className="Button Button__small" href="#">View all</a>
+          </div>
+        </div>
         <div>
           <div>Recent Transactions</div>
           <div>All Transactions</div>
